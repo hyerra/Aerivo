ab20d6bb767a173c32a16b7728098f7c93ec67e8
Harish Yerra <hyerra08@gmail.com>
2018-08-05 13:29:10 -0500

    Add basic AR functionality

---
+++ Aerivo/User Interface/Augmented Reality/ThresholdPanGesture.swift
@@ -0,0 +1,65 @@
+/*
+ Copyright © 2018 Apple Inc.
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
+import UIKit.UIGestureRecognizerSubclass
+
+/**
+ A custom `UIPanGestureRecognizer` to track when a translation threshold has been exceeded
+ and panning should begin.
+ 
+ - Tag: ThresholdPanGesture
+ */
+class ThresholdPanGesture: UIPanGestureRecognizer {
+    
+    /// Indicates whether the currently active gesture has exceeeded the threshold.
+    private(set) var isThresholdExceeded = false
+    
+    /// Observe when the gesture's `state` changes to reset the threshold.
+    override var state: UIGestureRecognizer.State {
+        didSet {
+            switch state {
+            case .began, .changed:
+                break
+                
+            default:
+                // Reset threshold check.
+                isThresholdExceeded = false
+            }
+        }
+    }
+    
+    /// Returns the threshold value that should be used dependent on the number of touches.
+    private static func threshold(forTouchCount count: Int) -> CGFloat {
+        switch count {
+        case 1: return 30
+            
+        // Use a higher threshold for gestures using more than 1 finger. This gives other gestures priority.
+        default: return 60
+        }
+    }
+    
+    /// - Tag: touchesMoved
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesMoved(touches, with: event)
+        
+        let translationMagnitude = translation(in: view).length
+        
+        // Adjust the threshold based on the number of touches being used.
+        let threshold = ThresholdPanGesture.threshold(forTouchCount: touches.count)
+        
+        if !isThresholdExceeded && translationMagnitude > threshold {
+            isThresholdExceeded = true
+            
+            // Set the overall translation to zero as the gesture should now begin.
+            setTranslation(.zero, in: view)
+        }
+    }
+}
