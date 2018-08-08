2d0ec6f2ac5724886c176e33f2490764c4ed8da7
Harish Yerra <hyerra08@gmail.com>
2018-08-07 21:44:12 -0500

    Fix collection view issues with dynamic type.

--- Aerivo/User Interface/Places/Detail/PlacesDetailViewController.swift
+++ Aerivo/User Interface/Places/Detail/PlacesDetailViewController.swift
@@ -450,7 +450,7 @@ class PlacesDetailViewController: UIViewController, UICollectionViewDataSource, 
                 alertController.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Title of cancel alert control action."), style: .cancel))
                 self.present(alertController, animated: true)
             } else {
-                let appName = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as! String
+                let appName = Bundle.main.localizedInfoDictionary?["CFBundleDisplayName"] as? String ?? Bundle.main.infoDictionary?["CFBundleDisplayName"] ?? "Aerivo"
                 let alertController = UIAlertController(title: NSLocalizedString("Oops 😣", comment: "Title of alert control for network error."), message: "\(appName) \(NSLocalizedString("is having trouble getting government representative information. This may be because the app doesn't have enough information about government officials for your area.", comment: "Message of alert controller for network error."))", preferredStyle: .alert)
                 alertController.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Title of cancel alert control action."), style: .cancel))
                 self.present(alertController, animated: true)
