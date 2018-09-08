5f1a5394cc888ad0972d46919dcc3a4358c00a1b
Harish Yerra <hyerra08@gmail.com>
2018-09-08 16:14:27 -0500

    Update cloud kit settings.

--- Aerivo WatchKit Extension/ExtensionDelegate.swift
+++ Aerivo WatchKit Extension/ExtensionDelegate.swift
@@ -19,6 +19,7 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
 
     func applicationDidBecomeActive() {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        CloudCore.fetchAndSave(to: DataController.shared.persistentContainer, error: nil, completion: nil)
     }
     
     func applicationWillResignActive() {
