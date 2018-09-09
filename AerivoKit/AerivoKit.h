5b839f49e06444f43df300ea93c30b931c5c9ad0
Harish Yerra <hyerra08@gmail.com>
2018-09-09 14:45:01 -0500

    Add macOS app target.

--- AerivoKit/AerivoKit.h
+++ AerivoKit/AerivoKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Harish Yerra. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for AerivoKit.
 FOUNDATION_EXPORT double AerivoKitVersionNumber;
