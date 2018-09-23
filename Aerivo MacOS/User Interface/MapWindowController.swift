49b3ff9f82a826fc56a45e08149f8b644f8b0ce1
Harish Yerra <hyerra08@gmail.com>
2018-09-22 21:36:10 -0500

    Add custom window controller.

---
+++ Aerivo MacOS/User Interface/MapWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MapWindowController.swift
+//  Aerivo MacOS
+//
+//  Created by Harish Yerra on 9/22/18.
+//  Copyright © 2018 Harish Yerra. All rights reserved.
+//
+
+import Cocoa
+
+class MapWindowController: NSWindowController {
+    
+    @IBOutlet weak var placesSearchField: NSSearchField!
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+}
